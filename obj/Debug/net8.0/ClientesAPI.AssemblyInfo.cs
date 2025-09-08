381cd8a78ad708a63b9e6240fee485ab51e12bb0
Felipe Bertelli dos Santos <42683582+fsbertelli@users.noreply.github.com>
2025-09-08 20:00:30 -0300

    Upload basic files

--- obj/Debug/net8.0/ClientesAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClientesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66eb0c5e3eae34a6829a177af2b5c5e72fc55050")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
